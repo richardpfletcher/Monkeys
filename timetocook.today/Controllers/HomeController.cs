7ba5f69ee35dd04c20f7f74468d930d21da3a10b
richardpfletcher <richardpfletcher@gmail.com>
2021-01-27 10:00:00 -0600

    added email

--- timetocook.today/Controllers/HomeController.cs
+++ timetocook.today/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using Cook.Models;
 using Cook.API;
+using System.Net.Mail;
+using heartoflove.Models;
 
 namespace timetocook.today.Controllers
 {
@@ -34,6 +36,86 @@ namespace timetocook.today.Controllers
 
             
         }
+
+
+        [ValidateInput(false)]
+        public ActionResult Comments(EmailModel model)
+        {
+            string emailLoggedIn = "";
+
+            try
+            {
+                if (Request.IsAuthenticated)
+                {
+                    //MembershipUser mu = Membership.GetUser();
+                    //emailLoggedIn = mu.UserName;
+                    emailLoggedIn = User.Identity.Name;
+                    model.Email = emailLoggedIn;
+                }
+            }
+            catch
+            {
+                emailLoggedIn = ""; //user logged in
+                //return View(); //user not logeed in
+            }
+
+            ViewData["emailLoggedIn"] = emailLoggedIn;
+
+            ViewData["message"] = "";
+
+            if (ModelState.IsValid)
+            {
+                CommentsPost(model);
+            }
+
+            return this.View(model);
+        }
+
+        //MenuPlanner
+
+        [ValidateInput(false)]
+
+        public ActionResult CommentsPost(EmailModel model)
+        {
+
+            SmtpClient smtpClient = new SmtpClient();
+            string EmailFrom = model.Email;
+            string EmailTo = "richardpfletcher@gmail.com";
+
+            try
+            {
+                using (var client = new SmtpClient("smtp.gmail.com"))
+                {
+                    using (var message = new MailMessage(EmailFrom, EmailTo))
+                    {
+                        message.Subject = "Time To Cook Comments from user " + model.Email;
+                        message.Body = model.Comments;
+                        MailAddress bcc = new MailAddress(EmailFrom);
+                        message.CC.Add(bcc);
+
+                        message.IsBodyHtml = true;
+                        client.UseDefaultCredentials = false;
+
+
+                        client.Credentials = new System.Net.NetworkCredential("richardpfletcher@gmail.com", "Barbara_1111");
+
+
+                        client.EnableSsl = true;
+                        client.Send(message);
+                    };
+                };
+            }
+            catch (Exception ex)
+            {
+                string x = ex.ToString();
+                throw;
+            }
+
+            ViewData["message"] = "Email is sent";
+
+            return RedirectToAction("Index", "Home");
+        }
+
 
         public ActionResult About()
         {
