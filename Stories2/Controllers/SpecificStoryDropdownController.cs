93ca159a34c9c923d7361d31d625e721cfd9495e
richardpfletcher <richardpfletcher@gmail.com>
2021-02-04 08:36:47 -0600

    imported project from jataka

---
+++ Stories2/Controllers/SpecificStoryDropdownController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http;
+using Stories.Factory;
+using System.Web.Http.Cors;
+
+namespace Stories.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class SpecificStoryDropdownController : ApiController
+    {
+
+        public response Get()
+        {
+
+            GetStories myStories = new GetStories();
+            return myStories.GetSpecificStoryDropdown();
+
+
+
+        }
+        
+    }
+}
---
+++ Stories2/Controllers/StoryCategorytNameController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Stories.Factory;
+using System.Web.Http.Cors;
+
+
+namespace Stories.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class StoryCategorytNameController : ApiController
+    {
+        [HttpGet]
+
+        public response Get()
+        {
+
+            GetStories myStories = new GetStories();
+            return myStories.GetStoryCategorytName();
+        }
+
+        [HttpGet]
+        [Route("api/StoryCategorytName/GetStoryCategoryNameByID")]
+
+
+        public response GetStoryCategoryNameByID(int id)
+
+        {
+
+            GetStories myStories = new GetStories();
+            response StoryCategoryName = myStories.GetStoryCategoryNameByID(id);
+
+            return StoryCategoryName;
+
+
+        }
+
+    }
+}
